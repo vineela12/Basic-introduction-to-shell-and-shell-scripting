25545f8b4c41100d387f04973b492bec1a674e36
Golkonda Vineela Mercy <vineelamercy@gmail.com>
2013-08-28 14:28:23 +0530

    simple commands

---
+++ shell/ss3.sh
@@ -0,0 +1,28 @@
+echo *
+echo D*
+echo *s
+echo ~
+echo ~foo
+
+
+echo $((2+2))
+echo $(($((5**2))*3))
+echo Five divided by two equals $((5/2))
+echo with $((5%2))
+
+echo Number - {1..6}
+echo a{A{1,2},B{3,4}}b
+echo Alphabets - {a..z}
+echo Alphabets - {A..Z}
+
+echo $USER
+echo $(ls)
+ls - l $(which cp)
+
+echo "$USER $((2+2)) $(cal)"
+
+
+Find the difference:-
+
+echo this is a   test
+echo "this is a     test"
